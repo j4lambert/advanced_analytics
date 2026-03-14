7af4241c7d465b6003874389b03e2d4ce38d97a0
stefanorigano (sri) <sri@odoo.com>
2026-03-14 18:54:32 +0100

    [IMP] ButtonsGroup design

--- src/components/buttons-group.jsx
+++ src/components/buttons-group.jsx
@@ -14,7 +14,7 @@ const { React } = api.utils;
  * @param {React.Node} props.children - ButtonsGroupItem components
  */
 export function ButtonsGroup({
-    groupClasses = 'bg-muted border inline-block items-center justify-center mx-auto p-1 rounded-md text-muted-foreground',
+    groupClasses = 'bg-muted border inline-flex items-center justify-center mx-auto p-1 rounded-md text-muted-foreground',
     value = null,
     onChange = () => {},
     children
@@ -79,7 +79,7 @@ export function ButtonsGroupItem({
         'aria-selected': active ? 'true' : 'false',
         'data-state': dataState,
         disabled: disabled ? 'true' : undefined,
-        className: 'aa-btn-group-btn inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
+        className: 'aa-btn-group-btn inline-flex items-center justify-center whitespace-nowrap rounded-sm px-2 py-1 text-xs font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
         tabIndex: '-1',
         onClick: handleClick
     }, text);
